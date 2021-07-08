8f09bfe86371c458c8bea628fdd2b6f68e65ec92
Upitroma <55750919+upitroma@users.noreply.github.com>
2021-07-07 19:08:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs')
+
+const PORT = 80
+
+var app = express();
+
+
+app.get("/api/:key/:content",function(request, response){
+	var key = request.params.key;
+	var content = request.params.content;
+    
+	//	var obj = { key : key, Content : "content " +idk };
+	if(key==12345){
+		if(content=="self"){
+			response.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
+    			response.write(fs.readFileSync('./index.js').toString());
+    			response.end();
+		}
+		else{
+			response.writeHead(404);
+			response.end();
+		}
+	}
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
