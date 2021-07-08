d42dabb924d170821139d2ac70d19c8c35e28911
upitroma <=>
2021-07-07 22:48:36 -0500

    lots of progress

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/api/:key/:cmd*",function(req, res){
 
 	//TODO: add slight random delay to stop timing attacks
 
+	//response
+	var resHeader;
+	var resBody;
+
 	//get keys from secrets.json
 	var raw = fs.readFileSync('secrets.json');
 	KEYS = JSON.parse(raw);
@@ -21,18 +25,50 @@ app.get("/api/:key/:cmd*",function(req, res){
 	var wildcard=req.params[0]
 
 	//check for valid key
+	var userkey;
+	var accessGranted=false
 	var keyArr = key.split(".");
 	for(i=0;i<KEYS.length;i++){
 		if(keyArr[0]==KEYS[i].id){
 			var hash = crypto.createHash('sha256').update(keyArr[1]).digest('hex');
 			if(hash==KEYS[i].keyHash){
-				console.log("access granted")
+				//check expiration date
+				if(new Date(KEYS[i].expiration)>new Date()){
+					console.log("access granted")
+
+					const response = execAPI(KEYS[i],cmd,wildcard)
+					res.writeHead(response.head.code,response.head.body);
+					res.write(response.body);
+					res.end();
+				}	
 				break
 			}
 		}
 	}
-    
-	//	var obj = { key : key, Content : "content " +idk };
+});
+
+function execAPI(key,cmd,wildcard){
+	if(cmd=="self" && key.perms.includes("dev")){
+		return{
+			head:{
+				code:200,
+				metadata:{"Content-Type": "text/plain; charset=UTF-8"}
+			},
+			body:fs.readFileSync('./index.js').toString()
+		}
+	}
+	
+	else{
+		return{
+			head:{
+				code:200,
+				metadata:{"Content-Type": "text/plain; charset=UTF-8"}
+			},
+			body:"Error: Invalid cmd or insufficient permissions"
+		}
+	}
+}
+	/*//	var obj = { key : key, Content : "content " +idk };
 	if(key==12345){
 		if(cmd=="self"){
 			res.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
@@ -47,7 +83,7 @@ app.get("/api/:key/:cmd*",function(req, res){
 		console.log(wildcard)
 	}
 });
-
+*/
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 })
